82479b555e1ae938e1ce28f81b0f5bb8b9b331cf
Yuyushiki <56354007+msthoshi@users.noreply.github.com>
2025-07-05 15:14:54 +0900

    Update pictureXXX_Ad_Close.user.js

--- pictureXXX_Ad_Close.user.js
+++ pictureXXX_Ad_Close.user.js
@@ -1,12 +1,15 @@
 // ==UserScript==
 // @name         pictureXXX Ad Close
-// @namespace    http://tampermonkey.net/
+// @namespace    https://github.com/msthoshi/UserScript
 // @version      2025-06-29
 // @description  pictureXXX Ad Close
 // @author       Yuyushiki
 // @match        https://picturexxx.space/*
 // @icon         https://picturexxx.space/content/images/system/default/favicon.png
 // @grant        none
+// @updateURL    https://github.com/msthoshi/UserScript/raw/main/pictureXXX_Ad_Close.user.js
+// @downloadURL  https://github.com/msthoshi/UserScript/raw/main/pictureXXX_Ad_Close.user.js
+// @supportURL   https://github.com/msthoshi/UserScript
 // ==/UserScript==
 
 (function() {
