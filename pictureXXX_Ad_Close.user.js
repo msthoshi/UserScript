4e74bb9eebd88e19adec99c8fa3d2af928de8865
Yuyushiki <56354007+msthoshi@users.noreply.github.com>
2025-07-05 15:28:15 +0900

    2025-07-05

--- pictureXXX_Ad_Close.user.js
+++ pictureXXX_Ad_Close.user.js
@@ -1,15 +1,16 @@
 // ==UserScript==
 // @name         pictureXXX Ad Close
 // @namespace    https://github.com/msthoshi/UserScript
-// @version      2025-06-29
+// @version      2025-07-05
 // @description  pictureXXX Ad Close
 // @author       Yuyushiki
+// @homepageURL  https://github.com/msthoshi/UserScript
+// @supportURL   https://github.com/msthoshi/UserScript
+// @updateURL    https://github.com/msthoshi/UserScript/raw/main/pictureXXX_Ad_Close.user.js
+// @downloadURL  https://github.com/msthoshi/UserScript/raw/main/pictureXXX_Ad_Close.user.js
 // @match        https://picturexxx.space/*
 // @icon         https://picturexxx.space/content/images/system/default/favicon.png
 // @grant        none
-// @updateURL    https://github.com/msthoshi/UserScript/raw/main/pictureXXX_Ad_Close.user.js
-// @downloadURL  https://github.com/msthoshi/UserScript/raw/main/pictureXXX_Ad_Close.user.js
-// @supportURL   https://github.com/msthoshi/UserScript
 // ==/UserScript==
 
 (function() {
