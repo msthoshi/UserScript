79e55cf279e79a0fa1e4cb448fd0eed82722af02
Yuyushiki <56354007+msthoshi@users.noreply.github.com>
2025-07-05 15:30:37 +0900

    2025-07-05

--- WikiPedia_Notice_Delete.user.js
+++ WikiPedia_Notice_Delete.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         WikiPedia Notice Delete
 // @namespace    https://github.com/msthoshi/UserScript
-// @version      2025-06-29
+// @version      2025-07-05
 // @description  WikiPedia Notice Delete.
 // @author       Yuyushiki
 // @homepageURL  https://github.com/msthoshi/UserScript
