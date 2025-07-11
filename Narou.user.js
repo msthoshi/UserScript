990d56de8c78c2958c68d4609a0f8c54ae3c1623
Yuyushiki <56354007+msthoshi@users.noreply.github.com>
2025-07-11 22:12:09 +0900

    2025-07-11 22:12

--- Narou.user.js
+++ Narou.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Narou JS
 // @namespace    https://github.com/msthoshi/UserScript
-// @version      2025-07-05
+// @version      2025-07-11
 // @description  Narou User JavaScript
 // @author       Yuyushiki
 // @homepageURL  https://github.com/msthoshi/UserScript
@@ -37,16 +37,79 @@
             }
         }
     }
+    function toHalfWidth(str) {
+        // 全角英数字を半角に変換
+        str = str.replace(/[Ａ-Ｚａ-ｚ０-９！？]/g, function(match) {
+            return String.fromCharCode(match.charCodeAt(0) - 0xFEE0);
+        });
+        return str;
+    }
+    function ReplaceHtml(strHtml){
+        var strTemp;
+        strTemp = strHtml.replace(/[！？]{2,}|[０-９]{2,}/g, function(match){
+            return(((/^(?:！！！|[！？]{2}|[０-９]{2})$/).test(match) || (/^(?:ｉｉ|ｉｉｉ|ｉｖ|ｖｉ|ｖｉｉ|ｖｉｉｉ|ｉｘ|ｘｉ|ｘｉｉ|ｘｉｉｉ|ＩＩ|ＩＩＩ|ＩＶ|ＶＩ|ＶＩＩ|ＶＩＩＩ|ＩＸ|ＸＩ|ＸＩＩ|ＸＩＩＩ)$/).test(match)) ? toHalfWidth(match) : match);
+        }).replace(/[0-9a-zA-Z#\$%&\\\!\?\/]+/g, function(match){
+            var str;
+            if (match.length == 1 || (/^(?:\!\!\!|[\!\?]{1,2}|[0-9]{1,2}|)$/).test(match) || (/^(?:ii|iii|iv|vi|vii|viii|ix|xi|xii|xiii)$/i).test(match)){
+                str = '<span class="tcy">' + match +'</span>';
+            } else {
+                if (match == '1/2'){
+                    str = '½';
+                } else if (match == '1/3'){
+                    str = '⅓';
+                } else if (match == '2/3'){
+                    str = '⅔';
+                } else if (match == '1/4'){
+                    str = '¼';
+                } else if (match == '3/4'){
+                    str = '¾';
+                } else if (match == '1/5'){
+                    str = '⅕';
+                } else if (match == '2/5'){
+                    str = '⅖';
+                } else if (match == '3/5'){
+                    str = '⅗';
+                } else if (match == '4/5'){
+                    str = '⅘';
+                } else if (match == '1/6'){
+                    str = '⅙';
+                } else if (match == '1/7'){
+                    str = '⅐';
+                } else if (match == '1/8'){
+                    str = '⅛';
+                } else if (match == '1/9'){
+                    str = '⅑';
+                } else if (match == '1/10'){
+                    str = '⅒';
+                } else {
+                    str = match;
+                }
+            }
+            return(str);
+        }).replace(/([^\x01-\x7E])[゛ﾞ]/g,'<span class="tcy">$1\u3099</span>').replace(/([^\x01-\x7E])[゜ﾟ]/g,'<span class="tcy">$1\u309A</span>');
+        return(strTemp);
+    }
     var arrHtml, i, element1, element2;
 
     window.addEventListener('keydown', fe, false);
 
-    arrHtml = document.querySelector('div[class="js-novel-text p-novel__text"]').innerHTML.replace(/(<ruby>[\s\S]*?<\/ruby>|<[^<>]+>)/g, '{split}$1{split}').split('{split}');
+    arrHtml = document.querySelector('h1.p-novel__title').innerHTML.replace(/(<ruby>[\s\S]*?<\/ruby>|<[^<>]+>)/g, '{split}$1{split}').split('{split}');
     if (arrHtml.length > 0){
         for (i=0; i < arrHtml.length; i++){
-            if (arrHtml[i].indexOf('<') == -1) arrHtml[i] = arrHtml[i].replace(/(?<=[^\x01-\x7E]|^)(\d{1,2})(?=[^\x01-\x7E]|$)/g, '<span class="tcy">$1</span>').replace(/(?<=[^\x01-\x7E]|^)([\!\?]{1,3})(?=[^\x01-\x7E]|$)/g, '<span class="tcy">$1</span>').replace(/(?<=[^\x01-\x7E]|^)([a-zA-Z\$\\\%\&])(?=[^\x01-\x7E]|$)/g, '<span class="tcy">$1</span>').replace(/([^\x01-\x7E])[゛ﾞ]/g,'<span class="tcy">$1\u3099</span>').replace(/([^\x01-\x7E])[゜ﾟ]/g,'<span class="tcy">$1\u309A</span>');
+            if (arrHtml[i].indexOf('<') == -1){
+                arrHtml[i] = ReplaceHtml(arrHtml[i]);
+            }
         }
+        document.querySelector('h1.p-novel__title').innerHTML = arrHtml.join('');
+    }
 
-        document.querySelector('div[class="js-novel-text p-novel__text"]').innerHTML = arrHtml.join('');
+    arrHtml = document.querySelector('div.p-novel__text').innerHTML.replace(/(<ruby>[\s\S]*?<\/ruby>|<[^<>]+>)/g, '{split}$1{split}').split('{split}');
+    if (arrHtml.length > 0){
+        for (i=0; i < arrHtml.length; i++){
+            if (arrHtml[i].indexOf('<') == -1){
+                arrHtml[i] = ReplaceHtml(arrHtml[i]);
+            }
+        }
+        document.querySelector('div.p-novel__text').innerHTML = arrHtml.join('');
     }
 })();
