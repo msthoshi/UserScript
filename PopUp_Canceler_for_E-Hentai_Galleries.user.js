215bce2a8890bae4637811f2cb8ed5aa8c136192
Yuyushiki <56354007+msthoshi@users.noreply.github.com>
2025-07-19 12:02:30 +0900

    2025-07-19T12:02:00

---
+++ PopUp_Canceler_for_E-Hentai_Galleries.user.js
@@ -0,0 +1,16 @@
+// ==UserScript==
+// @name         PopUp Canceler for E-Hentai Galleries
+// @namespace    https://github.com/msthoshi/UserScript
+// @version      2025-07-19T12:02:00
+// @description  PopUp Canceler for E-Hentai Galleries
+// @author       Yuyushiki
+// @homepageURL  https://github.com/msthoshi/UserScript
+// @supportURL   https://github.com/msthoshi/UserScript
+// @updateURL    https://github.com/msthoshi/UserScript/raw/main/PopUp_Canceler_for_E-Hentai_Galleries.user.js
+// @downloadURL  https://github.com/msthoshi/UserScript/raw/main/PopUp_Canceler_for_E-Hentai_Galleries.user.js
+// @match        https://e-hentai.org/*
+// @icon         https://e-hentai.org/favicon.ico
+// @grant        none
+// ==/UserScript==
+
+popUp = function (){return(false);}
