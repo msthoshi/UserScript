98dffbb5d10497055464a81a4674c375770d2121
Yuyushiki <56354007+msthoshi@users.noreply.github.com>
2025-08-18 23:29:12 +0900

    2025-08-18T23:29:00

--- FileName_Auto_Copy_in_E-Hentai_Galleries.user.js
+++ FileName_Auto_Copy_in_E-Hentai_Galleries.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         FileName Auto Copy in E-Hentai Galleries
 // @namespace    https://github.com/msthoshi/UserScript
-// @version      2025-08-07T18:13:00
+// @version      2025-08-18T23:29:00
 // @description  FileName Auto Copy in E-Hentai Galleries
 // @author       Yuyushki
 // @homepageURL  https://github.com/msthoshi/UserScript
@@ -14,19 +14,16 @@
 // ==/UserScript==
 
 (function() {
-    var arrMatch, strGallerieID, intMaxLen, strNum, strFileName;
+    var strURL, arrMatch, strFileName;
     function PasteFileName(){
         navigator.clipboard.writeText(strFileName);
         document.querySelector("#img").removeEventListener('contextmenu', PasteFileName, false);
         document.querySelector("#i6").removeEventListener('contextmenu', PasteFileName, false);
     }
-    arrMatch = document.URL.match(/(\d+)-\d+$/);
-    strGallerieID = (arrMatch) ? arrMatch[1] : '';
-    intMaxLen = document.querySelector('#i2 > div.sn > div > span:nth-child(2)').innerHTML.length;
-    arrMatch = document.URL.match(/\d+$/);
-    strNum = (arrMatch) ? arrMatch[0] : '';
-    if (strGallerieID && strNum){
-        strFileName = strGallerieID + '-' + ('0000' + strNum).slice(intMaxLen * -1);
+    strURL = document.URL;
+    arrMatch = strURL.replace(/\?[\s\S]+$/, '').match(/(\d+)-(\d+)$/);
+    if (arrMatch){
+        strFileName = arrMatch[1] + '-' + ('0000' + arrMatch[2]).slice(document.querySelector('#i2 > div.sn > div > span:nth-child(2)').innerHTML.length * -1);
         document.querySelector("#img").addEventListener('contextmenu', PasteFileName, false);
         document.querySelector("#i6").addEventListener('contextmenu', PasteFileName, false);
     }
